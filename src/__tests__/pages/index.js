cd7242f17968cbf188fc38f48bc3e553971f9c25
Pierre Haller <pierre@marmelab.com>
2019-06-26 17:43:03 +0200

    Add linter and prettier

--- src/__tests__/pages/index.js
+++ src/__tests__/pages/index.js
@@ -1,22 +1,22 @@
-import React from "react";
-import Home from "../../pages/index";
-import { render, cleanup } from "@testing-library/react";
+import React from 'react';
+import Home from '../../pages/index';
+import { render, cleanup } from '@testing-library/react';
 
 afterEach(cleanup);
 
-describe("HomePage tests", () => {
-  it("should show a welcome message", () => {
-    const { getByText } = render(<Home />);
+describe('HomePage tests', () => {
+    it('should show a welcome message', () => {
+        const { getByText } = render(<Home />);
 
-    const isWelcomePresent = getByText(/Quixo game with React & NextJS !/);
+        const isWelcomePresent = getByText(/Quixo game with React & NextJS !/);
 
-    expect(isWelcomePresent).toBeTruthy();
-  });
-  it("should show a button to play a new game", () => {
-    const { getByText } = render(<Home />);
+        expect(isWelcomePresent).toBeTruthy();
+    });
+    it('should show a button to play a new game', () => {
+        const { getByText } = render(<Home />);
 
-    const isNewGamePresent = getByText(/New Game !/);
+        const isNewGamePresent = getByText(/New Game !/);
 
-    expect(isNewGamePresent).toBeTruthy();
-  });
+        expect(isNewGamePresent).toBeTruthy();
+    });
 });
--- src/pages/game.js
+++ src/pages/game.js
@@ -1,53 +1,48 @@
-import styled from "styled-components";
+import React from 'react';
+import styled from 'styled-components';
 
 const Board = styled.div`
-  width: 100%;
-  height: 100%;
-  background-image: url("/static/board.jpg");
-  background-size: 100% 100%;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  flex-direction: column;
+    width: 100%;
+    height: 100%;
+    background-image: url('/static/board.jpg');
+    background-size: 100% 100%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
 `;
 
 const Row = styled.div`
-  flex-direction: row;
-  display: flex;
+    flex-direction: row;
+    display: flex;
 `;
 
 const Cube = styled.div`
-  display: inline-block;
+    display: inline-block;
 `;
 
 const CubeImage = styled.img`
-  width: 100px;
-  height: 100px;
+    width: 100px;
+    height: 100px;
 `;
 
 const Game = ({ board }) => (
-  <Board>
-    {board.map((row, x) => (
-      <Row key={`row-${x}`}>
-        {row.map((value, y) => (
-          <Cube key={`cube-${x}-${y}`} title={`cube-${x}-${y}`}>
-            <CubeImage src={"/static/neutral.png"} />
-          </Cube>
+    <Board>
+        {board.map((row, x) => (
+            <Row key={`row-${x}`}>
+                {row.map((value, y) => (
+                    <Cube key={`cube-${x}-${y}`} title={`cube-${x}-${y}`}>
+                        <CubeImage src={'/static/neutral.png'} />
+                    </Cube>
+                ))}
+            </Row>
         ))}
-      </Row>
-    ))}
-  </Board>
+    </Board>
 );
 
-Game.getInitialProps = async ({ req }) => {
-  const mockedBoard = [
-    [0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0]
-  ];
-  return { board: mockedBoard };
+Game.getInitialProps = async () => {
+    const mockedBoard = [[0, 0, 0, 0, 0], [0, 0, 0, 0, 0], [0, 0, 0, 0, 0], [0, 0, 0, 0, 0], [0, 0, 0, 0, 0]];
+    return { board: mockedBoard };
 };
 
 export default Game;
