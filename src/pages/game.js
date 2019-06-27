7c2849a6e4db743479dff4544865369515b5db0c
Pierre Haller <pierre@marmelab.com>
2019-06-27 15:18:48 +0200

    Add instructions

--- src/pages/game.js
+++ src/pages/game.js
@@ -1,5 +1,37 @@
 import React from 'react';
 import styled from 'styled-components';
+import { getNewGame, getExistingGame, getMyTeam } from '../apiRequests';
+import Instructions from '../components/Instructions';
+
+const isMyTurn = (myTeam, currentPlayer) => myTeam === currentPlayer;
+
+const Game = ({ game, team }) => {
+    const { board, winner, currentPlayer } = game;
+    const isPlaying = isMyTurn(team, currentPlayer);
+    return (
+        <Board>
+            <Instructions team={team} isPlaying={isPlaying} winner={winner} />
+            {board.map((row, x) => (
+                <Row key={`row-${x}`}>
+                    {row.map((value, y) => (
+                        <Cube key={`cube-${x}-${y}`} title={`cube-${x}-${y}`}>
+                            <CubeImage src={'/static/neutral.png'} />
+                        </Cube>
+                    ))}
+                </Row>
+            ))}
+        </Board>
+    );
+};
+
+Game.getInitialProps = async ({ query }) => {
+    const { id } = query;
+
+    const game = id ? await getExistingGame(id) : await getNewGame();
+    const { team } = await getMyTeam(game.id);
+
+    return { game, team };
+};
 
 const Board = styled.div`
     width: 100%;
@@ -7,7 +39,6 @@ const Board = styled.div`
     background-image: url('/static/board.jpg');
     background-size: 100% 100%;
     display: flex;
-    justify-content: center;
     align-items: center;
     flex-direction: column;
 `;
@@ -25,24 +56,5 @@ const CubeImage = styled.img`
     width: 100px;
     height: 100px;
 `;
-
-const Game = ({ board }) => (
-    <Board>
-        {board.map((row, x) => (
-            <Row key={`row-${x}`}>
-                {row.map((value, y) => (
-                    <Cube key={`cube-${x}-${y}`} title={`cube-${x}-${y}`}>
-                        <CubeImage src={'/static/neutral.png'} />
-                    </Cube>
-                ))}
-            </Row>
-        ))}
-    </Board>
-);
-
-Game.getInitialProps = async () => {
-    const mockedBoard = [[0, 0, 0, 0, 0], [0, 0, 0, 0, 0], [0, 0, 0, 0, 0], [0, 0, 0, 0, 0], [0, 0, 0, 0, 0]];
-    return { board: mockedBoard };
-};
 
 export default Game;
